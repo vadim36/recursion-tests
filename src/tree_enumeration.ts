8883ba3f1d82836124ba6b37d85604744190d027
Vadim Medvedev <vadim130809@gmail.com>
2024-01-14 10:58:40 +0300

    add tree_enumeration script

---
+++ src/tree_enumeration.ts
@@ -0,0 +1,46 @@
+type TTreeBranchObject = {
+  [index:string]:number
+}
+
+type TTemplateObject = {
+  [index:string]: TTreeBranchObject
+}
+
+class NodeElement {
+  constructor (
+    readonly body:number,
+    public childrens?:NodeElement[]
+  ) {}
+}
+
+function getTreeNodes(objectTMP:TTemplateObject):NodeElement[] {
+  const treeBranches:TTreeBranchObject[] = Object.values(objectTMP);
+  const nodes: NodeElement[] = [];
+
+  treeBranches.forEach((branch:TTreeBranchObject, branchIndex:number):number => {
+    return nodes.push(...getNodes(branchIndex, treeBranches));
+  });
+
+  return nodes;
+}
+
+function getNodes(levelIndex:number, branchesArray:TTreeBranchObject[]):NodeElement[] {
+  const nodes: NodeElement[] = [];
+  const currentBranch = Object.values(branchesArray[levelIndex]);
+
+  currentBranch.forEach((nodeBody:number):number => {
+    const node: NodeElement = new NodeElement(nodeBody);
+
+    if (levelIndex + 1 !== branchesArray.length) {
+      node.childrens = getNodes(levelIndex + 1, branchesArray);
+    }
+
+    return nodes.push(node);
+  });
+
+  return nodes;
+}
+
+module.exports = {
+  getTreeNodes
+}
---
+++ tests/tree_enumeration.test.ts
@@ -0,0 +1,14 @@
+const treeEnumerationModule = require('../src/tree_enumeration.ts');
+const getTreeNodes = treeEnumerationModule.getTreeNodes;
+
+describe('getTreeNodes', () => {
+  //let testObject: ;
+
+  afterEach(() => {
+    //testObject;
+  });
+
+  test('Correct One-level Object', () => {
+  
+  });
+});
